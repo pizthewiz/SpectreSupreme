1b67377161e118842343d7d7fb5e7d4762806b0b
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-06-13 20:54:29 -0700

    add skeleton for new webkit/svg rendering patch

---
+++ SpectreSupreme/SpectreSupremePlugin.h
@@ -0,0 +1,12 @@
+//
+//  SpectreSupremePlugin.h
+//  SpectreSupreme
+//
+//  Created by Jean-Pierre Mouilleseaux on 13 Jun 2011.
+//  Copyright 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface SpectreSupremePlugIn : QCPlugIn {}
+@end
